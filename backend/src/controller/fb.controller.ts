6d6f6a9f5cf6bb6a0c848f261ba535e97f364ea0
dashainCodes <dashain2.07@gmail.com>
2024-04-16 12:41:48 +0545

    final

--- backend/src/controller/fb.controller.ts
+++ backend/src/controller/fb.controller.ts
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/appError";
@@ -23,105 +22,112 @@ import nodemailer from "nodemailer";
 import UserModel from "../models/user.model";
 import crypto from "crypto";
 import { uploadSingleFile } from "../middleware/uploadSingleFile";
-const axios = require('axios');
-const { createClient } = require('@supabase/supabase-js');
+const axios = require("axios");
+const { createClient } = require("@supabase/supabase-js");
 const supabaseUrl = "https://hxkfcxzouhixqcbjidis.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4a2ZjeHpvdWhpeHFjYmppZGlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzMjc4NTAsImV4cCI6MjAyNjkwMzg1MH0.7YAj-6vGndKU8uaQp8pd7AqbKKuUmcfEaTL4rb2zZVk";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function fbLogInHandler(
-    req: Request<{}, {}, CreateUserInput["body"]>,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const existingUserWithEmail = await findUser({ email: req.body.email });
-      if (existingUserWithEmail) {
-        const accessToken = jwt.sign(
-          {user:existingUserWithEmail},
-          `${process.env.AUTH_SECRET_KEY}`,
-          {
-            expiresIn: "1d",
-          }
-        );
-        console.log("user",existingUserWithEmail);
-        console.log("token",accessToken);
-        return res.status(201).json({
-          status: "success",
-          msg: "Log in success",
-          accessToken: accessToken,
-        });
-      }
-  
-      const token = crypto.randomBytes(20).toString("hex");
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const existingUserWithEmail = await findUser({ email: req.body.email });
+    if (existingUserWithEmail) {
       const accessToken = jwt.sign(
-        {user:existingUserWithEmail},
+        { user: existingUserWithEmail },
         `${process.env.AUTH_SECRET_KEY}`,
         {
           expiresIn: "1d",
         }
       );
-      console.log("token",accessToken);
-      const createdUser = await createUser({
-        ...req.body,
-        verifyToken: token,
-        isVerified: true,
-      });
-      console.log("token:", token);
+      console.log("user", existingUserWithEmail);
+      console.log("token", accessToken);
       return res.status(201).json({
         status: "success",
-        msg: "Register success",
-        data: createdUser,
+        msg: "Log in success",
         accessToken: accessToken,
       });
-    } catch (error: any) {
-      console.error(colors.red("msg:", error.message));
-      next(new AppError("Internal server error", 500));
     }
-  }
 
- 
-  export async function getAccessTokenFromCode(req: Request, res: Response, next: NextFunction) {
-   try {
-    const code=req.params.code
-   try {
-    const { data } = await axios({
-      url: 'https://graph.facebook.com/v4.0/oauth/access_token',
-      method: 'get',
-      params: {
-        client_id: "746525737287945",
-        client_secret: "bfd4f4091fe3b43bc0c7959f036d6632",
-        redirect_uri: "https://expert-vercel.vercel.app/profile",
-        code,
-      },
+    const token = crypto.randomBytes(20).toString("hex");
+   
+    const createdUser = await createUser({
+      ...req.body,
+      verifyToken: token,
+      isVerified: true,
     });
-    console.log(data); 
+    const accessToken = jwt.sign(
+      { user: createdUser },
+      `${process.env.AUTH_SECRET_KEY}`,
+      {
+        expiresIn: "1d",
+      }
+    );
+    console.log("token:", token);
     return res.status(201).json({
       status: "success",
       msg: "Register success",
-      
-      accessToken: data.access_token,
+      data: createdUser,
+      accessToken: accessToken,
     });
-   } catch (error:any) {
+  } catch (error: any) {
     console.error(colors.red("msg:", error.message));
-    next(new AppError(error.message, 500));
-   }
-   } catch (error:any) {
-    console.error(colors.red("msg:", error.message));
-    next(new AppError(error.message, 500));
-   }
-  };
+    next(new AppError("Internal server error", 500));
+  }
+}
 
-  export async function getFacebookUserData(req: Request, res: Response, next: NextFunction) {
+export async function getAccessTokenFromCode(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const code = req.params.code;
     try {
-      const access_token=req.params.access_token
-     try {
       const { data } = await axios({
-        url: 'https://graph.facebook.com/me',
-        method: 'get',
+        url: "https://graph.facebook.com/v4.0/oauth/access_token",
+        method: "get",
         params: {
-          fields: ['id', 'email', 'first_name', 'last_name'].join(','),
+          client_id: "746525737287945",
+          client_secret: "bfd4f4091fe3b43bc0c7959f036d6632",
+          redirect_uri: "https://expert-vercel.vercel.app/profile",
+          code,
+        },
+      });
+      console.log(data);
+      return res.status(201).json({
+        status: "success",
+        msg: "Register success",
+
+        accessToken: data.access_token,
+      });
+    } catch (error: any) {
+      console.error(colors.red("msg:", error.message));
+      next(new AppError(error.message, 500));
+    }
+  } catch (error: any) {
+    console.error(colors.red("msg:", error.message));
+    next(new AppError(error.message, 500));
+  }
+}
+
+export async function getFacebookUserData(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const access_token = req.params.access_token;
+    try {
+      const { data } = await axios({
+        url: "https://graph.facebook.com/me",
+        method: "get",
+        params: {
+          fields: ["id", "email", "first_name", "last_name"].join(","),
           access_token: access_token,
         },
       });
@@ -129,15 +135,14 @@ export async function fbLogInHandler(
       return res.status(201).json({
         status: "success",
         msg: "Register success",
-        data:data
+        data: data,
       });
-     } catch (error:any) {
-      console.error(colors.red("msg:", error.message));
-      next(new AppError(error.message, 500));
-     }
-    } catch (error:any) {
+    } catch (error: any) {
       console.error(colors.red("msg:", error.message));
       next(new AppError(error.message, 500));
     }
-  };
-  
+  } catch (error: any) {
+    console.error(colors.red("msg:", error.message));
+    next(new AppError(error.message, 500));
+  }
+}
